f7c033cbbda6801857d038e5426c55ba90f3631b
stevejkang <iam@juneyoung.io>
2026-08-12 05:59:37 +0900

    Rewrite in Go with launchd daemon support

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/stevejkang/remote-mac-lock
+
+go 1.26.5
--- src/server.ts
+++
@@ -1,6 +0,0 @@
-import App from './App.js';
-import ExecuteScreenLock from './controller/ExecuteScreenLock.js';
-
-const app = new App([new ExecuteScreenLock()]);
-
-app.listen();
